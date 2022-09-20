2ae9406c2d82290049ec9d2917c13f4bbddded0c
umitkoc <umitkoc.com@gmail.com>
2022-09-20 21:36:12 +0300

    code init heroku

--- api.py
+++ api.py
@@ -1,31 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 import translators as ts
 from flask_cors import CORS, cross_origin
 from requests import get
 from bs4 import BeautifulSoup
-
-
 app = Flask(__name__)
 cors = CORS(app, resources={r"/foo": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-
-@app.route('/<text>',methods=["POST"])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@app.route('/<text>', methods=["POST"])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def index(text):
-    return ts.google(from_language="en",to_language="tr",query_text=text)
+    return ts.google(from_language="en", to_language="tr", query_text=text)
+
+
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+
+
 @app.route('/randomtext')
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def text():
-    response=get("https://randomword.com/paragraph")
-    text=BeautifulSoup(response.text,'html.parser')
+    response = get("https://randomword.com/paragraph")
+    text = BeautifulSoup(response.text, 'html.parser')
     return text.select(selector='#random_word_definition')[0].text
-
-
-
-if "__main__"==__name__:
-    app.run(debug=True)
