e458d963320cb425ca3951de3113972a245f9d26
Ümit KOC <umitkoc.com@gmail.com>
2022-09-23 20:15:26 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 import translators as ts
-from flask_cors import CORS, cross_origin
+#from flask_cors import CORS, cross_origin
 from requests import get
 from bs4 import BeautifulSoup
 app = Flask(__name__)
-cors = CORS(app, resources={r"/foo": {"origins": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
+#cors = CORS(app, resources={r"/foo": {"origins": "*"}})
+#app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/<text>', methods=["POST"])
-@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+#@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def index(text):
     return ts.google(from_language="en", to_language="tr", query_text=text)
 
@@ -20,7 +20,7 @@ def home():
 
 
 @app.route('/randomtext')
-@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+#@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def text():
     response = get("https://randomword.com/paragraph")
     text = BeautifulSoup(response.text, 'html.parser')
